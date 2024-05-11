0335d865d764abd3796a7ad872c8b34496f17edd
kniknivadim <137409582+kniknivadim@users.noreply.github.com>
2024-05-11 23:22:40 +0500

    Add files via upload

---
+++ .github/workflows/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const isLoggedIn = true; // Допустим, пользователь успешно вошел
+
+    if (isLoggedIn) {
+        // Отобразить меню для скачивания файлов
+        document.querySelector('.menu').style.display = 'block';
+
+        // Добавляем обработчики событий для каждой кнопки "Скачать"
+        document.getElementById('download1').addEventListener('click', function() {
+            window.location.href = 'https://drive.usercontent.google.com/u/0/uc?id=1ej-EZq22e6tUNRNKuyNL3ivlKoXu634G&export=download';
+        });
+
+        document.getElementById('download2').addEventListener('click', function() {
+            window.location.href = 'ссылка на файл 2';
+        });
+
+        document.getElementById('download3').addEventListener('click', function() {
+            window.location.href = 'ссылка на файл 3';
+        });
+
+        document.getElementById('download4').addEventListener('click', function() {
+            window.location.href = 'ссылка на файл 4';
+        });
+
+        document.getElementById('download5').addEventListener('click', function() {
+            window.location.href = 'ссылка на файл 5';
+        });
+    }
+});    
